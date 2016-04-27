898a9a62dcb2a7344afdd8c37a797ca1c615376e
johntalbotcox@gmail.com <talbotcox@Talbots-MacBook-Pro.local>
2016-04-27 16:56:47 -0600

    Little presentation

---
+++ app.js
@@ -0,0 +1,17 @@
+(function() {
+    angular.module('InspirationalApp', ["ui.router","controllers"])
+        .config(routerConfig)
+        
+        function routerConfig($stateProvider,$urlRouterProvider){
+            
+            $stateProvider
+                .state('home',{
+                    url: "/",
+                    templateUrl: "home.html"
+                    
+                })
+                
+                $urlRouterProvider.otherwise('/')
+                
+        }
+}())
